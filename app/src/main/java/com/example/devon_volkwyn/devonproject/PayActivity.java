e97470fac158cc02e279a1af343749481d95f0ab
Devon.Volkwyn <devtuks@gmail.com>
2017-10-05 09:41:30 +0200

    App in final form
    Commenting added, code completed and all functionalities working as intended to.

--- app/src/main/java/com/example/devon_volkwyn/devonproject/PayActivity.java
+++ app/src/main/java/com/example/devon_volkwyn/devonproject/PayActivity.java
@@ -13,35 +13,48 @@ import android.widget.Toast;
 
 public class PayActivity extends AppCompatActivity {
 
+    // Button method to update the hourly rate as intended.
     public void clickCalculate(View view){
+
+        // Retrieving the content from the layout in order to modify it.
         EditText txtHours = (EditText) findViewById(R.id.txtHours);
         String txtHoursString = txtHours.getText().toString();
+
+        // Shows a toast message to the user if space is left blank
         if (txtHoursString.matches("")) {
             Toast.makeText(this, "You did not enter the amount of hours worked!", Toast.LENGTH_SHORT).show();
             return;
         }
+
+        // Converting a String to a double.
         Double numHour = Double.parseDouble(txtHoursString);
 
+        // Retrieving the content from the layout in order to modify it.
         EditText txtRate = (EditText) findViewById(R.id.txtRate);
         String txtRateString = txtRate.getText().toString();
+
+        // Shows a toast message to the user if space is left blank
         if (txtRateString.matches("")) {
             Toast.makeText(this, "You did not enter the hourly rate in rands!", Toast.LENGTH_SHORT).show();
             return;
         }
+
+        // Converting a String to a double.
         Double numRate = Double.parseDouble(txtRateString);
 
         //For testing purposes
         Log.i("Info", "Amount of hours worked = " + txtHoursString);
         Log.i("Info", "Hourly rate = " + txtRateString);
 
+        // Retrieving the content from the layout in order to modify it.
         TextView lblResult = (TextView) findViewById(R.id.lblResult);
-
         ConstraintLayout contLay = (ConstraintLayout) findViewById(R.id.contLay);
 
         // Hide the keyboard from the screen when the button is clicked.
         InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(contLay.getWindowToken(), 0);
 
+        // Performing checks and modifying the hourly rate of pay as intended.
         if (numHour == 40 && numRate < 28.50){
             numRate = numRate + 1.50;
             lblResult.setText(String.format("R" + "%.2f",numRate));
